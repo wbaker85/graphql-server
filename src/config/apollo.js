1b23bc06a03475cf1161d47eee9337f655ff20d7
William Baker <wbaker@gmail.com>
2020-10-26 17:58:00 -0400

    subscription with rabbitmq

--- src/config/apollo.js
+++ src/config/apollo.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from 'apollo-server';
 import { RedisCache } from 'apollo-server-cache-redis';
+import responseCachePlugin from 'apollo-server-plugin-response-cache';
 
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
@@ -7,9 +8,18 @@ import resolvers from './resolvers';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [responseCachePlugin()],
   cache: new RedisCache({
     host: 'localhost',
   }),
+  cacheControl: {
+    defaultMaxAge: 20,
+  },
+  persistedQueries: {
+    cache: new RedisCache({
+      host: 'localhost',
+    }),
+  },
 });
 
 export default server;
